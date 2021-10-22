91dd82753685b031c448eb002034c7246ad0de44
Case Delst <casedelst@gmail.com>
2021-10-21 23:21:15 -0700

    more work

--- client/src/App.js
+++ client/src/App.js
@@ -6,8 +6,6 @@ function App() {
   return (
     <div className="App">
       <Title/>
-
-      <div>hello!</div>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
--- client/src/Title.jsx
+++ client/src/Title.jsx
@@ -1,12 +1,15 @@
 // src/Title.js
 
 import React from 'react'
+import './Title.css';
 
 function Title() {
     return (
-        <div>
-            <h1>SFPOPOS</h1>
+        <div className="Title">
+            <h1>Case Delst</h1>
+            <div className="Title-Subtitle"> A Software Engineer</div>
         </div>
+
     )
 }
 
